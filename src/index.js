742e28fd1232f09c52d5c9f866fb8fb3d4fbd5e4
Kuba <jkb.kam@gmail.com>
2018-05-02 12:00:34 +0200

    displays who is next

--- src/index.js
+++ src/index.js
@@ -2,27 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-
-    render() {
+function Square(props){
         return (
             <button className="square"
-                    onClick={() => this.props.onClick()}>
-                {this.props.value}
+                    onClick={() => props.onClick()}>
+                {props.value}
             </button>
         );
     }
-}
+
 
 class Board extends React.Component {
     state= {
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        xIsNext: true
     };
 
-    handleClick(i) {
+    handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i]= 'X';
-        this.setState({squares: squares});
+        squares[i]= this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,});
     }
 
     renderSquare(i) {
@@ -31,7 +32,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
